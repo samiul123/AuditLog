a29441ac60c0f07c349e027dc6112389dfce62c3
Sami <samiulmushfik123@gmail.com>
2021-12-30 01:58:46 +0600

    Initial Setup

---
+++ backend/src/main/java/com/amag/interview/auditlog/AuditLogApplication.java
@@ -0,0 +1,13 @@
+package com.amag.interview.auditlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuditLogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuditLogApplication.class, args);
+    }
+
+}
