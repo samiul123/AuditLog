1df21f184d41dc4b16a93b8c515c79b43d093219
Sami <samiulmushfik123@gmail.com>
2022-01-09 01:44:04 +0600

    Add Circular progress

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,14 @@
 import React, {Suspense} from "react";
 import './App.css';
 import {BrowserRouter, Route} from "react-router-dom";
+import {CircularProgress} from "@mui/material";
 
 function App() {
     const Login = React.lazy(() => import("./component/login/Login"))
     const Layout = React.lazy(() => import("./component/common/Layout"))
     return (
         <BrowserRouter>
-            <Suspense fallback={<div>Loading...</div>}>
+            <Suspense fallback={<CircularProgress/>}>
                 <Route path='/login' name='Login' component={Login}/>
                 <Route path='/' component={Layout}/>
             </Suspense>
