a3bc1c3650e9d59baa32befe9b348de9edf7f136
Sami <samiulmushfik123@gmail.com>
2022-01-09 02:10:31 +0600

    Add css to center circular progress bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,18 @@
 import React, {Suspense} from "react";
 import './App.css';
 import {BrowserRouter, Route} from "react-router-dom";
-import {CircularProgress} from "@mui/material";
+import {Box, CircularProgress} from "@mui/material";
 
 function App() {
     const Login = React.lazy(() => import("./component/login/Login"))
     const Layout = React.lazy(() => import("./component/common/Layout"))
     return (
         <BrowserRouter>
-            <Suspense fallback={<CircularProgress/>}>
+            <Suspense fallback={
+                <Box sx={{display: "flex", justifyContent: "center", alignItems: "center", height: "100vh"}}>
+                    <CircularProgress/>
+                </Box>
+            }>
                 <Route path='/login' name='Login' component={Login}/>
                 <Route path='/' component={Layout}/>
             </Suspense>
